df8b93e73e4ebb036fb8531bc4e6ad2d5ee4ae1e
JohinsDev <johinsdev@gmail.com>
2018-07-04 18:22:08 -0500

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import { SAVE_COMMENT } from './types';
+
+export function saveComment(comment) {
+  return {
+    type: SAVE_COMMENT,
+    payload: comment
+  };
+}
---
+++ src/reducers/comments.js
@@ -0,0 +1,10 @@
+import { SAVE_COMMENT } from '../actions/types';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case SAVE_COMMENT:
+      return [...state, action.payload];
+    default: 
+      return state;
+  }
+}
